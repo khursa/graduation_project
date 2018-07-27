66d2005a31f8aa58d4f6f7005e249c0109c8350e
Khursa <khursa@protonmail.com>
2018-07-27 03:27:50 +0300

    fix title of 'activity' block

--- domsystem/src/components/Activity/Activity_title/index.js
+++ domsystem/src/components/Activity/Activity_title/index.js
@@ -2,51 +2,37 @@ import React, { Component } from 'react';
 import '../../../menu.js';
 import './style.css'
 
-class About__history extends Component {
+class Activity_title extends Component {
   render() {
     return (
       <div 
-      className="about__history section" 
+      className="activity__wrap section" 
       id="2"
       >
-        <div className="history__wrap">
-          <div className="history__info">
-            <History__text  
-            history__text__1='Робота iз кабельними системами будь-якої складностi, оснащення серверних, побудова локальної мережі, доступ до мережі Інтернет
-            Широкий спектр виконувальних робiт: системи Wireless, ВОЛ3, СКУД, IP-вiдеоспостереження, супутникове обладнання, автоматизація, сонячне електроживлення, електромонтаж, система "розумний будинок"
-            Власний штат досвідченних iнженерiв, висококвалiфiкованих монтажникiв та колл-центр, який працює для Вас без вихідних'
-            history__text__2="Використання лише надійних та перевiрених матерiалів та обладнання
-            Додатковi послуги та сервiси, гарантійне обслуговування
-            Виготовлення власної продукції
-            Вигідні та зручні для Вас умови cпiвпpaцi
-            20 рiчний досвід успішної роботи та безлiч кейсiв!"
-            />          
+          <div className="activity__info">
+            <Activity_header  
+            activity__title='НАПРЯМКИ ДIЯЛЬНОСТI'
+
+            />    
+            <p className="activity__text">
+            Діяльність ТОВ "ДОМАШНІ СИСТЕМИ" охоплює широкий спектр виконуваних робiт, 
+            спрямований на створення комплексних рішень в галузі інформаційних технологій, 
+            побудову складних взаємопов'язаних масштабованих систем функціонування автоматизованих процесів, 
+            та ітнерації їх в одну єдину інформаційно-комунікаційну інфраструктуру
+            </p>  
           </div>
-        </div>
       </div>
     );
   }
 }
 
-const History__title = (props) => {
+const Activity_header = (props) => {
   return (
-     <h2 className="history__title">
-        { props.history__title }     
+     <h2 className="activity__title">
+        { props.activity__title }     
     </h2>
   )
 }
 
-const History__text = (props) => {
-  return (
-    <div>
-      <p className="history__text">
-          { props.history__text__1 }     
-      </p>
-      <p className="history__text">
-          { props.history__text__2 }     
-      </p>
-    </div>  
-  )
-}
 
-export default About__history;
+export default Activity_title;
